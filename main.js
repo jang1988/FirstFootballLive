bc3280607adde1871732bdbca0331ef4e4775620
Baklay Petro <pustota1988@gmail.com>
2022-08-27 13:02:49 +0300

    FIrstFootballLive

---
+++ main.js
@@ -0,0 +1,29 @@
+// 112, 44, 4,
+const elapsedTime = document.querySelector('#elapsed');
+const homeTeamImage = document.querySelector('#homeLogo');
+const homeTeamName = document.querySelector('#homeName');
+const awayTeamImage = document.querySelector('#awayLogo');
+const awayTeamName = document.querySelector('#awayName');
+const lastMatchGoal = document.querySelector('#goals');
+const matchTable = document.querySelector('#matchTable');
+const container = document.querySelector('.container');
+
+fetch(
+  'https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=1d0ac0c32b6676f830a158103842dccfe320ff44b529ddc4bee8962d9ca9953c'
+)
+  .then((response) => response.json())
+  .then((response) => {
+    const matchList = response.result;
+    matchList.forEach((element) => {
+      console.log('element: ', element);
+      const containerClone = container.cloneNode(true);
+      elapsedTime.textContent = element.event_status;
+      homeTeamName.textContent = element.event_home_team;
+      awayTeamName.textContent = element.event_away_team;
+      homeTeamImage.src = element.home_team_logo;
+      awayTeamImage.src = element.away_team_logo;
+      lastMatchGoal.innerHTML = element.event_final_result;
+      document.body.prepend(containerClone);
+    });
+  })
+  .catch((err) => console.error(err));
