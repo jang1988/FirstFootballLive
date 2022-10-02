f7fb5982a9f020ad59b42feadd2089ec06929405
Baklay Petro <pustota1988@gmail.com>
2022-10-02 18:25:54 +0300

    standing1

--- js/standing.js
+++ js/standing.js
@@ -1,3 +1,6 @@
+// #3 - UEFA Champions League
+// #4 - UEFA Europa League
+
 const key = '2fa03fe1199351c4797529ca86b95fb6326c7b53c8c601b3b7c3a3d8eec97c1f';
 
 let url = `https://apiv2.allsportsapi.com/football/?&met=Standings&leagueId=3&APIkey=${key}`;
@@ -7,4 +10,5 @@ fetch(url)
   .then((response) => {
     const result = response.result
     console.log(result);
+    
   });
