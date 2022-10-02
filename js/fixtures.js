10f97a50dcfd909410750474b001ac19d60fe2f7
Baklay Petro <pustota1988@gmail.com>
2022-10-02 17:38:32 +0300

    FixturesTwoPage

--- js/fixtures.js
+++ js/fixtures.js
@@ -1,22 +1,80 @@
-const options = {
-  key: '2fa03fe1199351c4797529ca86b95fb6326c7b53c8c601b3b7c3a3d8eec97c1f',
-  day: '1',
-  month: '1',
-  year: '2022',
-};
+const key = '2fa03fe1199351c4797529ca86b95fb6326c7b53c8c601b3b7c3a3d8eec97c1f';
+let day = '1';
+let month = '1';
+let year = '2022';
+let url = `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${key}&from=${year}-${month}-${day}&to=${year}-${month}-${day}`;
 
-options.day = new Date().getDate()
-options.month = new Date().getMonth()
-options.year = new Date().getFullYear()
+day = new Date().getDate();
+month = new Date().getMonth();
+year = new Date().getFullYear();
 
+const calBtn = document.querySelector('.calendar-btn');
+const calendar = document.querySelector('#month-calendar');
+console.log(day);
 
-fetch(
-  `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${options.key}&from=${options.year}-${options.month}-${options.day}&to=${options.year}-${options.month}-${options.day}`
-)
+const dateNumber = document.querySelector('#date');
+dateNumber.textContent = day;
+const monthNumber = document.querySelector('#month');
+monthNumber.textContent = month;
+const yearNumber = document.querySelector('#year');
+yearNumber.textContent = year;
+
+calendar.lastElementChild.addEventListener('click', (e) => {
+  if (e.target.textContent) {
+    e.target.style.color = getRandomColor();
+
+    day = e.target.textContent;
+    console.log('day: ', day);
+    fetch(
+      `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${key}&from=${year}-${month}-${day}&to=${year}-${month}-${day}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const allMatch = response.result;
+        console.log('allMatch: ', allMatch);
+
+        const oldContent = document.body.children;
+
+        for (const item of oldContent) {
+          if (item.className === 'fixtures-wrraper') {
+            item.innerHTML = '';
+            dateNumber.textContent = day;
+          }
+        }
+
+        const templateContent = document.querySelector('#fixtures').innerHTML;
+        const fixturesWrraper = document.querySelector('.fixtures-wrraper');
+
+        allMatch.forEach((match) => {
+          let html = Mustache.render(templateContent, match);
+          fixturesWrraper.insertAdjacentHTML('beforeend', html);
+        });
+
+        const matchContent = document.querySelectorAll('.match-content');
+        matchContent.forEach((div) => {
+          div.addEventListener('click', showStat);
+        });
+
+        function showStat() {
+          this.children[1].classList.toggle('active__fixtures');
+        }
+
+        console.log('day: ', day);
+      })
+      .catch((err) => console.error(err));
+  }
+});
+
+calBtn.addEventListener('click', (e) => {
+  e.target.parentElement.nextElementSibling.classList.toggle('hidden');
+});
+
+// LIVE FETCH
+fetch(url)
   .then((response) => response.json())
-  .then((response) => {
+  .then(function (response) {
     const allMatch = response.result;
-    console.log('response: ', response)
+    console.log('response: ', response);
     console.log('allMatch: ', allMatch);
 
     const templateContent = document.querySelector('#fixtures').innerHTML;
@@ -37,9 +95,13 @@ fetch(
     }
   })
   .catch((err) => console.error(err));
+// LIVE FETCH END
 
-  const calBtn = document.querySelector('.calendar-btn')
-
-  calBtn.addEventListener('click', (e) => {
-    e.target.nextElementSibling.classList.toggle('hidden')
-  })
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
